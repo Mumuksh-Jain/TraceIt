b1de0fa542a1572f88aa1fa88f0cc5e1e5a6c9cf
Moksh <mumukshujain2466@gmail.com>
2026-03-22 19:04:42 +0530

    fix : Cors Issue

--- backend-node/src/app.js
+++ backend-node/src/app.js
@@ -5,10 +5,27 @@ const lostitemRoutes=require("./routes/lostitem.route")
 const foundItemRoutes=require("./routes/foundItem.route")
 const app=express();
 const cors=require("cors")
-app.use(cors({
-    origin:['http://localhost:5173','http://localhost:3001'],
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://trace-it-nu.vercel.app/"
+      ];
+
+      if (
+        !origin || 
+        allowedOrigins.includes(origin) ||
+        origin.endsWith(".vercel.app")
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  })
+);
 
 app.use(express.json())
 app.use(cookieParser())
