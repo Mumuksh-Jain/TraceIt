374ac301da52b6c92c35a94a794b52f3b3a7c8e6
Moksh <mumukshujain2466@gmail.com>
2026-03-24 09:57:06 +0530

    Port Issues Fixed

--- backend-python/app.py
+++ backend-python/app.py
@@ -33,6 +33,5 @@ def match():
 @app.route("/health")
 def health():
     return "OK", 200
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
